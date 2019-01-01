aaa040116e71d522019619dea539f6aae51c205d
wq1019 <2013855675@qq.com>
2019-01-02 00:09:07 +0800

    移除软删除特性

--- model/file.go
+++ model/file.go
@@ -5,16 +5,15 @@ import (
 )
 
 type File struct {
-	Id        int64      `gorm:"primary_key" json:"id"`                 // ID
-	Folders   []*Folder  `gorm:"many2many:folder_files;" json:"-"`      // 仅仅用来作为关联,而不需要显示 所以用 - 忽略
-	Filename  string     `gorm:"not null" json:"filename"`              // 文件名称
-	Hash      string     `gorm:"type:varchar(32);not null" json:"hash"` // 文件Hash
-	Format    string     `gorm:"not null" json:"format"`                // 文件MimeType 例如: video/mp4 -> .mp4
-	Extra     string     `json:"extra" gorm:"not null;type:text"`       // extra
-	Size      int64      `gorm:"type:bigint" json:"size"`               // 文件大小
-	CreatedAt time.Time  `json:"created_at"`                            // 创建时间
-	UpdatedAt time.Time  `json:"updated_at"`                            // 更新时间
-	DeletedAt *time.Time `sql:"index" json:"deleted_at"`                // 软删除时间
+	Id        int64     `gorm:"primary_key" json:"id"`                 // ID
+	Folders   []*Folder `gorm:"many2many:folder_files;" json:"-"`      // 仅仅用来作为关联,而不需要显示 所以用 - 忽略
+	Filename  string    `gorm:"not null" json:"filename"`              // 文件名称
+	Hash      string    `gorm:"type:varchar(32);not null" json:"hash"` // 文件Hash
+	Format    string    `gorm:"not null" json:"format"`                // 文件MimeType 例如: video/mp4 -> .mp4
+	Extra     string    `json:"extra" gorm:"not null;type:text"`       // extra
+	Size      int64     `gorm:"type:bigint" json:"size"`               // 文件大小
+	CreatedAt time.Time `json:"created_at"`                            // 创建时间
+	UpdatedAt time.Time `json:"updated_at"`                            // 更新时间
 }
 
 type FileStore interface {
--- model/folder.go
+++ model/folder.go
@@ -3,17 +3,16 @@ package model
 import "time"
 
 type Folder struct {
-	Id         int64      `gorm:"primary_key" json:"id"`                                     // ID
-	Files      []*File    `gorm:"many2many:folder_files;" json:"files"`                      // many2many
-	Folders    []*Folder  `gorm:"foreignkey:ParentId" json:"folders"`                        // one2many 当前目录下的目录
-	UserId     int64      `gorm:"index:user_id_folder_name_unique_index" json:"user_id"`     // 创建者(组合唯一)
-	FolderName string     `gorm:"index:user_id_folder_name_unique_index" json:"folder_name"` // 目录名称(组合唯一)
-	ParentId   int64      `gorm:"default:0" json:"parent_id"`                                // 父目录
-	Key        string     `gorm:"default:''" json:"key"`                                     // 辅助键
-	Level      int64      `gorm:"default:1" json:"level"`                                    // 辅助键
-	CreatedAt  time.Time  `json:"created_at"`                                                // 创建时间
-	UpdatedAt  time.Time  `json:"updated_at"`                                                // 更新时间
-	DeletedAt  *time.Time `json:"deleted_at"`                                                // 软删除时间
+	Id         int64     `gorm:"primary_key" json:"id"`                                     // ID
+	Files      []*File   `gorm:"many2many:folder_files;" json:"files"`                      // many2many
+	Folders    []*Folder `gorm:"foreignkey:ParentId" json:"folders"`                        // one2many 当前目录下的目录
+	UserId     int64     `gorm:"index:user_id_folder_name_unique_index" json:"user_id"`     // 创建者(组合唯一)
+	FolderName string    `gorm:"index:user_id_folder_name_unique_index" json:"folder_name"` // 目录名称(组合唯一)
+	ParentId   int64     `gorm:"default:0" json:"parent_id"`                                // 父目录
+	Key        string    `gorm:"default:''" json:"key"`                                     // 辅助键
+	Level      int64     `gorm:"default:1" json:"level"`                                    // 辅助键
+	CreatedAt  time.Time `json:"created_at"`                                                // 创建时间
+	UpdatedAt  time.Time `json:"updated_at"`                                                // 更新时间
 }
 
 const (
