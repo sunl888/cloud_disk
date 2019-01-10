368b5c26b82d311da784f60848d6cfb556ca76fe
wq1019 <2013855675@qq.com>
2019-01-10 11:50:13 +0800

    重写 Byte 格式化成 KB, MB, GB, TB 的代码

--- handler/me.go
+++ handler/me.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/wq1019/cloud_disk/handler/middleware"
 	"github.com/wq1019/cloud_disk/model"
+	"github.com/wq1019/cloud_disk/pkg/bytesize"
 	"github.com/wq1019/cloud_disk/service"
 	"github.com/zm-dev/go-image_uploader/image_url"
 	"net/http"
@@ -13,13 +14,6 @@ import (
 type meHandler struct {
 	imageUrl image_url.URL
 }
-
-const (
-	KB = int64(1024)
-	MB = KB << 10
-	GB = MB << 10
-	TB = GB << 10
-)
 
 func (m *meHandler) Show(c *gin.Context) {
 	uid := middleware.UserId(c)
@@ -43,27 +37,11 @@ func convert2UserResp(user *model.User, imageUrl image_url.URL) map[string]inter
 		"avatar_url":        imageUrl.Generate(user.UserInfo.AvatarHash),
 		"profile":           user.UserInfo.Profile,
 		"email":             user.UserInfo.Email,
-		"used_storage":      TransformByte(user.UserInfo.UsedStorage),
+		"used_storage":      bytesize.ByteSize(user.UserInfo.UsedStorage),
 		"group_name":        user.Group.Name,
-		"max_allow_storage": TransformByte(user.Group.MaxStorage),
+		"max_allow_storage": bytesize.ByteSize(user.Group.MaxStorage),
 		"is_allow_share":    user.Group.AllowShare,
 	}
-}
-
-func TransformByte(byteInt int64) (data string) {
-	byteFloat := float64(byteInt)
-	if byteInt >= TB {
-		data = fmt.Sprintf("%.3fTB", byteFloat/1024/1024/1024/1024)
-	} else if byteInt >= GB {
-		data = fmt.Sprintf("%.3fGB", byteFloat/1024/1024/1024)
-	} else if byteInt >= MB {
-		data = fmt.Sprintf("%.3fMB", byteFloat/1024/1024)
-	} else if byteInt >= KB {
-		data = fmt.Sprintf("%.3fKB", byteFloat/1024)
-	} else {
-		data = fmt.Sprintf("%.0fB", byteFloat)
-	}
-	return
 }
 
 func NewMeHandler(imageUrl image_url.URL) *meHandler {
---
+++ pkg/bytesize/bytesize.go
@@ -0,0 +1,34 @@
+package bytesize
+
+import (
+	"fmt"
+)
+
+const (
+	_          = iota // ignore first value by assigning to blank identifier
+	KB float64 = 1 << (10 * iota)
+	MB
+	GB
+	TB
+	PB
+	EB
+)
+
+func ByteSize(i uint64) string {
+	b := float64(i)
+	switch {
+	case b >= EB:
+		return fmt.Sprintf("%.2fEB", b/EB)
+	case b >= PB:
+		return fmt.Sprintf("%.2fPB", b/PB)
+	case b >= TB:
+		return fmt.Sprintf("%.2fTB", b/TB)
+	case b >= GB:
+		return fmt.Sprintf("%.2fGB", b/GB)
+	case b >= MB:
+		return fmt.Sprintf("%.2fMB", b/MB)
+	case b >= KB:
+		return fmt.Sprintf("%.2fKB", b/KB)
+	}
+	return fmt.Sprintf("%dB", i)
+}
