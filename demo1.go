0f75d0055ce072bbeb78a5ab887b55cf49b8aa7d
wq1019 <2013855675@qq.com>
2019-01-10 12:10:09 +0800

    测试代码

--- demo1.go
+++ demo1.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"strconv"
 	"strings"
 )
@@ -9,29 +10,44 @@ import (
 func main() {
 	folderMap := make(map[int64]string, 5)
 	folderMap[1] = "根目录"
-	folderMap[2] = "目录2"
-	folderMap[3] = "目录3"
-	folderMap[4] = "目录4"
-	folderMap[5] = "目录5"
+	folderMap[2] = "1.1"
+	folderMap[3] = "1.1.1"
+	folderMap[4] = "1.1.1.1"
+	folderMap[5] = "1.1.2"
+	folderMap[6] = "1.1.2.1"
 
-	key := "1-2-3-4-5-"
-	currentKey := int64(2)
-	path := generatePath(folderMap, currentKey, key)
+	key := "1-2-3-"
+	path := mergePath(folderMap, 1, key, 4)
 	fmt.Println(path)
+
+	tmp := "bytes=0-"
+	prefixIndex := strings.Index(tmp, "-")
+	fmt.Println(prefixIndex)
+	start, err := strconv.ParseInt(tmp[6:prefixIndex], 10, 64)
+	if err != nil {
+		log.Fatal(err.Error())
+		return
+	}
+	fmt.Println(tmp[prefixIndex+1:] == "")
+	end, err := strconv.ParseInt(tmp[prefixIndex+1:], 10, 64)
+	if err != nil {
+		log.Fatal(err.Error())
+		return
+	}
+	fmt.Println(start, end)
 }
-func generatePath(folderMap map[int64]string, currentId int64, key string) (path string) {
-	flag := false
+
+func mergePath(folderMap map[int64]string, currentId int64, key string, withId int64) (path string) {
+	if currentId == withId {
+		return ""
+	}
 	key2Arr := strings.Split(key, "-")
 	for _, v := range key2Arr {
 		id2Int64, _ := strconv.ParseInt(v, 10, 64)
-		if id2Int64 == currentId {
-			flag = true
-		}
-		if flag {
+		if id2Int64 > currentId {
 			path += folderMap[id2Int64] + "/"
 		}
 	}
-	firstIndex := strings.Index(path, "/")
-	lastIndex := strings.LastIndex(path, "/")
-	return path[firstIndex+1 : lastIndex-1]
+	path += folderMap[withId]
+	return path
 }
